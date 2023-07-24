8dabacfcef4f85eb6d1e096604348bb7d7e8b5d0
Amir_Razer <amirmahmoed003@gmail.com>
2023-07-24 20:35:36 +0700

    menampilkan data buku

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -2,14 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Book;
 use Illuminate\Http\Request;
 
 class BookController extends Controller
 {
      public function index()
     {
+        $books = Book::all();
         // $request->session()->flush();
         // dd('ini halaman buku');
-        return view('book');
+        return view('book',['books'=>$books]);
     }
 }
--- resources/views/book.blade.php
+++ resources/views/book.blade.php
@@ -3,7 +3,35 @@
 @section('title', 'book')
 
 @section('content')
-<h1>
-    Ini halaman books    
-</h1>
+        <h1>
+            Books List   
+        </h1>
+        <div class="my-5">
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th>No.</th>
+                        <th>Code</th>
+                        <th>Title</th>
+                        <th>Status</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($books as $item)
+                        <tr>
+                            <td>{{$loop->iteration}}</td>
+                            <td>{{$item->book_code}}</td>
+                            <td>{{$item->title}}</td>
+                            <td>{{$item->status}}</td>
+                             <td>
+                        <a href="#">Edit</a>
+                        <a href="#">Delet</a>
+                    </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+  
     @endsection
